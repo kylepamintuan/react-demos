a52248ecb87673a18b07d59caff2d67324a4d2e1
Kyle Pamintuan <kylepamintuan@gmail.com>
2018-09-12 17:57:22 -0700

    second demo finished.

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
+import Greeting from './greeting';
 
 const App = () => (
     <div>
         <div className="app">
             <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
+            <h1>Welcome to React!</h1>
+            <Greeting name="John Doe" favColor="red"/>
+            <Greeting name="John Doe" favColor="grey"/>
         </div>
     </div>
 );
---
+++ src/components/greeting.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Greeting(props){
+    const style = { color: props.favColor };
+
+    return <h1 style={style}>Hello, {props.name}</h1>;
+}
+
+export default Greeting;
