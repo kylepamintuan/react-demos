d11a684d3bb7603488bd6da9a6f3ac71fc0964ad
Kyle Pamintuan <kylepamintuan@gmail.com>
2018-09-17 16:21:04 -0700

    finished challenge.

--- src/components/sign_in.js
+++ src/components/sign_in.js
@@ -10,6 +10,9 @@ class SignInForm extends Component {
                 password: ''
             }
         }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
     }
 
     render(){
@@ -17,14 +20,14 @@ class SignInForm extends Component {
         return (
             <div>
                 <h1>Demo Form</h1>
-                <form onSubmit={(event) => {this.handleFormSubmit(event)}}>
+                <form onSubmit={this.handleFormSubmit}>
                     <div>
                         <label>Email </label>
-                        <input type="text" name="email" value={email} onChange={ (event) => this.handleChange(event) } />   
+                        <input type="text" name="email" value={email} onChange={this.handleChange} />   
                     </div><br/>
                     <div>
                         <label>Password </label>
-                        <input type="password" name="password" value ={password} onChange={ (event) => this.handleChange(event) } />   
+                        <input type="password" name="password" value={password} onChange={this.handleChange} />   
                     </div><br/>
                 <input type="submit" value="Submit" />
                 </form>
@@ -34,16 +37,21 @@ class SignInForm extends Component {
 
     handleChange(event){
         const { name, value } = event.target;
-
         const { form } = this.state;
-
-        this.setState({form: {[name]: value}});
+        this.setState({form: {...form, [name]: value}});
     }
 
     handleFormSubmit(event){
         event.preventDefault();
-        console.log('Form Submitted:', event.target);
-        console.dir(event);
+        const { form } = this.state;
+        console.log('Form Submitted:', form);
+
+        this.setState( {           
+            form: {
+                email: '',
+                password: ''
+            }
+        });
     }
 }
 
